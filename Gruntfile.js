2d24bbc97a26ed71214a8a49e4db958ee3b9458f
Dan Leatherman <dleatherman@gmail.com>
2014-09-11 23:15:26 -0400

    add minification and other grunt modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -274,6 +274,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-mysql-dump-import');
 
